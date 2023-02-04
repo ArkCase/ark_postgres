63cc905f8be25cdacfba6f2ef9b05922d960401c
Diego Rivera <diego.rivera@armedia.com>
2023-02-04 16:55:02 -0600

    Fixed a typo

--- ark_pentaho_ee/ark_pentaho_ee.sh
+++ ark_pentaho_ee/ark_pentaho_ee.sh
@@ -1,4 +1,25 @@
-psql -U ${PGUSER} -d ${POSTGRESQL_DATABASE} -a -f /opt/app-root/src/ark_pentaho_ee/create_quartz_postgresql.sql
-psql -U ${PGUSER} -d ${POSTGRESQL_DATABASE} -a -f /opt/app-root/src/ark_pentaho_ee/create_repository_postgresql.sql
-psql -U ${PGUSER} -d ${POSTGRESQL_DATABASE} -a -f /opt/app-root/src/ark_pentaho_ee/create_jcr_postgresql.sql
-psql -U ${PGUSER} -d ${POSTGRESQL_DATABASE} -a -f /opt/app-root/src/ark_pentaho_ee/pentaho_mart_postgresql.sql
+#!/bin/bash
+
+say() {
+	echo -e "${@}"
+}
+
+fail() {
+	say "${@}" 1>&2
+	exit ${EXIT_CODE:-1}
+}
+
+[ -v PGUSER ] || PGUSER="postgres"
+SRC_DIR="/opt/app-root/src/ark_pentaho_ee"
+
+SCRIPTS=(
+	"${SRC_DIR}/create_quartz_postgresql.sql"
+	"${SRC_DIR}/create_repository_postgresql.sql"
+	"${SRC_DIR}/create_jcr_postgresql.sql"
+	"${SRC_DIR}/pentaho_mart_postgresql.sql"
+)
+
+for SQL in "${SCRIPTS[@]}" ; do
+	psql -U "${PGUSER}" -a -f "${SQL}" || fail "Failed to execute the initialzation script [${SSQL}]"
+done
+exit 0
